67b37d2db62c06c3ac77f174d9cba79cd538a5ab
James <77841651+ChirawatEmphan@users.noreply.github.com>
2024-06-15 13:34:52 +0700

    Add files via upload

---
+++ attendance.sql
@@ -0,0 +1,2 @@
+INSERT INTO `attendance` (`id`, `user_name`, `attendance_time`) VALUES (1, 'James', '2024-05-30 15:17:00');
+INSERT INTO `attendance` (`id`, `user_name`, `attendance_time`) VALUES (2, 'JPD', '2025-07-16 16:29:00');
